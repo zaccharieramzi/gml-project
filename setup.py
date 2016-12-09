b9e1a9ba03e4f4e7e937ecec70088cc74fb2cb18
zaccharieramzi <zaccharie.ramzi@gmail.com>
2016-12-09 18:07:57 +0100

    created dev environment with setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = "correlation clustering",
+    version = "1.0.0",
+    author = "Zaccharie Ramzi and Pierre Dubreuil",
+    author_email = "zaccharie.ramzi@teelcom-paristech.fr , pierre.dubreuil@telecom-paristech.fr",
+    description = ("Grpahs in Machine Learning Project"),
+    license = "BSD",
+    keywords = "correlation clustering graphs",
+    packages=['correlation_clustering'],
+)
