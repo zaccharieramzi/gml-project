d6dffdde7fa6fcc75ba1f3acd799452be50e2a5e
zaccharieramzi <zaccharie.ramzi@gmail.com>
2016-12-17 18:06:28 +0100

    modified optimization problem to SDP relaxation with triangular inequalities

--- correlation_clustering/algo.py
+++ correlation_clustering/algo.py
@@ -3,48 +3,66 @@ import numpy as np
 import picos as pic
 
 
-def build_laplacian(w):
-    ''' Out of the adjacency matrix, builds the laplacian of the graph
+def solve_sdp(w, d=None, solver='cvxopt'):
+    ''' Solves the SDP relaxation of the max-cut problem associated with the
+    graph w.
         Args:
             - w (np.matrix): the adjacency matrix
         Outputs:
-            - np.matrix: the laplacian of the graph
-    '''
-    degrees = np.sum(W, axis=1)
-    d = np.diag(degrees)  # the degree matrix
-    return d - w
-
-
-def solve_sdp(l):
-    ''' Solves the SDP relaxation of the max-cut problem associated with the
-    laplacian l.
-        Args:
-            - l (np.matrix) : the laplacian of the graph
-        Outputs:
             - np.matrix : the solution of the SDP
     '''
-    n = l.shape[0]
+    n = w.shape[0]
+    if not(d):
+        d = n
     prob = pic.Problem()
-    x = prob.add_variable('x', (n, n), vtype='symmetric')
-    prob.set_objective('min', x | l)
-    prob.add_constraint(x >> 0)
+    v = prob.add_variable('v', (n, d))
+    node_couples = [(i, j) for i in range(n) for j in range(n)]
+    node_triples = [
+        (i, j, k) for i in range(n) for j in range(n) for k in range(n)]
+    prob.set_objective('max', pic.sum(
+        [w[e]*pic.norm(v[:, e[0]]-v[:, e[1]], 2)**2) for e in node_couples],
+        [('e', 2)],
+        'node couples'
+        )
+    # All indicators on unit sphere
     prob.add_list_of_constraints(
-        [x[i, i] == 1 for i in range(n)],
+        [pic.norm(v[:, i])**2 == 1 for in range(n)],
         'i',
-        '[n]')
-    return prob.solve(solver='cvxopt', verbose=0)
+        'nodes')
+
+    def sqdist(u, v):
+        return pic.norm(u - v)**2
+
+    # First triangular inequality
+    prob.add_list_of_constraints(
+        [sqdist(v[:, t[0]], v[:, t[1]]) + sqdist(v[:, t[1]], v[:, t[2]]) >=
+         sqdist(v[:, t[0]], v[:, t[2]]) for t in node_triples],
+        't',
+        'node triples')
+    # Second triangular inequality
+    prob.add_list_of_constraints(
+        [sqdist(v[:, t[0]], v[:, t[1]]) + sqdist(v[:, t[1]], -v[:, t[2]]) >=
+         sqdist(v[:, t[0]], -v[:, t[2]]) for t in node_triples],
+        't',
+        'node triples')
+    # Third triangular inequality
+    prob.add_list_of_constraints(
+        [sqdist(v[:, t[0]], -v[:, t[1]]) + sqdist(v[:, t[1]], -v[:, t[2]]) >=
+         sqdist(v[:, t[0]], v[:, t[2]]) for t in node_triples],
+        't',
+        'node triples')
+    return prob.solve(solver=solver, verbose=0)
 
 
 def assignment_solution(x):
     ''' Checks whether the solution returned by the SDP is integral, and if it
-    is, returns the assignment defined by x.
+    is, returns the assignment defined by v.
         Args:
-            - x (n.matrix): the solution of the SDP
+            - v (n.matrix): the solution of the SDP
         Outputs:
             - list of int: assignment for each node to a certain cluster if
                 solution is integral, False otherwise.
     '''
-    v = np.linalg.cholesky(x)
     vectors = set()
     for i in range(v.shape[0]):
         vectors.add(v[:, i])
