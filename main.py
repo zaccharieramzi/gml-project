b3fac244e8285ba427297fcee68ebf5b6ad42fcf
pdubreuil1 <pierre.dubreuil@telecom-paristech.fr>
2017-01-02 22:27:37 +0100

    modified data_prox and main to consider the case where we subsample the number of users

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ summary_dictionary = build_user_to_movies('movielens.tsv')
 # unpacking of the dictionary
 users_to_movies = summary_dictionary['users_to_movies']
 n_users = summary_dictionary['n_users']
+k_users = n_users / 2
 n_movies = summary_dictionary['n_movies']
 # building the rating matrix
-a = build_a(n_users, n_movies, users_to_movies)
+a = build_a(n_users, k_users, n_movies, users_to_movies)
 # building the adjacency matrix
-W = build_graph(n_users, n_movies, a)
+W = build_graph(k_users, n_movies, a)
 # the laplacian of the graph
 L = build_laplacian(W)
 X = solve_sdp(W)
